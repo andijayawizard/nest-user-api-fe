e586512dd0b8bd3d6a5fac7b1563736bf0256e3e
andijayawizard <andijayawizard@gmail.com>
2023-04-11 16:10:31 +0700

    1104

--- src/app/components/user/user-add/user-add.component.ts
+++ src/app/components/user/user-add/user-add.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @angular-eslint/use-lifecycle-interface */
 import { User } from '@/interfaces/user';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '@services/user/user.service';
 
@@ -10,22 +11,29 @@ import { UserService } from '@services/user/user.service';
   styleUrls: ['./user-add.component.scss']
 })
 export class UserAddComponent implements OnInit {
+  userForm:FormGroup
   title:string='add new user'
   user:User={
     firstName: '',
     lastName: '',
   }
   submitted:boolean=false
-  constructor(private service:UserService,private router:Router){}
-  ngOnInit():void{}
+  constructor(private service:UserService,private router:Router,private fb:FormBuilder){}
+  ngOnInit():void{
+    this.userForm=this.fb.group({
+      firstName:[''],
+      lastName:['']
+    })
+  }
   storeUser():void{
     const data={
       firstName:this.user.firstName,
       lastName:this.user.lastName
     }
-    this.service.createUser(data).subscribe({
+    this.service.createUser(this.userForm.value).subscribe({
       next:(res:any)=>{
-        console.log(res)
+        console.log('insert success')
+        this.router.navigateByUrl('/user')
         // this.submitted=true
         // this.router.navigate(['/user'])
       },
