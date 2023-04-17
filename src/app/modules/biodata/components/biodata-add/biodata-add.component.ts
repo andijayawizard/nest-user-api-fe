14815f44f801a670aef38611c95baf803f05c02c
andijayawizard <andijayawizard@gmail.com>
2023-04-17 13:10:50 +0700

    1704

--- src/app/modules/biodata/components/biodata-add/biodata-add.component.ts
+++ src/app/modules/biodata/components/biodata-add/biodata-add.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BiodataService } from '@modules/biodata/biodata.service';
 
 @Component({
   selector: 'app-biodata-add',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./biodata-add.component.scss']
 })
 export class BiodataAddComponent {
-
+  articleForm:FormGroup
+  constructor(private service:BiodataService, private router:Router, private fb:FormBuilder){}
+  ngOnInit():void{
+    
+  }
+  create():void{
+    this.service.create(this.articleForm.value).subscribe({
+      next:()=>{
+        console.log('insert success')
+        this.router.navigateByUrl('biodata')
+      }
+    })
+  }
 }
