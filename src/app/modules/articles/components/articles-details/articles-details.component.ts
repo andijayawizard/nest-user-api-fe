f07efe863ca64e7731f21a3484ccb8d8dc76c64a
andijayawizard <andijayawizard@gmail.com>
2023-04-17 09:48:25 +0700

    1704

--- src/app/modules/articles/components/articles-details/articles-details.component.ts
+++ src/app/modules/articles/components/articles-details/articles-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticlesService } from '@modules/articles/articles.service';
 
 @Component({
   selector: 'app-articles-details',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./articles-details.component.scss']
 })
 export class ArticlesDetailsComponent {
-
+  title:string='Articles'
+  details:any
+  constructor(private service:ArticlesService, private route:ActivatedRoute){}
+  ngOnInit():void{this.findOne()}
+  findOne():void{
+    const id:string=this.route.snapshot.params['id']
+    this.service.findOne(id).subscribe({
+      next:(res:any)=>{
+        this.details=res
+      }
+    })
+  }
 }
